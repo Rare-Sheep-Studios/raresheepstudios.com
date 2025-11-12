0198604432e5d991367bd53191f4b08eb2fa1cdf
Vaughn <drkel.public.dev@proton.me>
2025-11-12 00:43:57 -0500

    work PLSSSS

--- pages/mainsite/contact/people/js/people.js
+++ pages/mainsite/contact/people/js/people.js
@@ -55,7 +55,12 @@ function initSearchAnimation() {
     searchInput.addEventListener('blur', () => {
         searchInput.parentElement.classList.remove('focused');
     });
+<<<<<<< HEAD
         // Add input event listener to trigger search when user types
+=======
+    
+    // Add input event listener to trigger search when user types
+>>>>>>> 84d62bd (work PLSSSS)
     searchInput.addEventListener('input', () => {
         searchPeople();
     });
